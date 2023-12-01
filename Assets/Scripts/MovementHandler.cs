f3066309a6fa1e375d0f0c0b7bf61b320388a38e
decendium <54756899+decendium@users.noreply.github.com>
2023-11-30 21:28:34 -0500

    Movement and Models
    - added fences around the map
    - added a hat onto player
    - added movement into the game (basic)

--- Assets/Scripts/MovementHandler.cs
+++ Assets/Scripts/MovementHandler.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class MovementHandler : MonoBehaviour
 {
+    [Header("Movement")]
+    public float moveSpeed = 5f;
 
-    void Start()
+    Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    void Update()
+    private void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.fixedDeltaTime;
+
+        rb.MovePosition(transform.position + movement);
     }
 }
